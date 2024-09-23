62e0b22f0c8c822bf67064b45ea208550d0e268c
Zeeta:) <126560008+zzz3t444@users.noreply.github.com>
2024-09-23 18:56:36 +0700

    commit

--- src/section/Banner/Banner.svelte
+++ src/section/Banner/Banner.svelte
@@ -6,7 +6,7 @@
    <div>
     <img title="Product Us" class=" w-[1200px] border dark:border-neutral-800 relative -mt-52 rounded-3xl mx-auto" src="https://basehub.com/_next/image?url=https%3A%2F%2Fbasehub.earth%2F9dbc0da9%2FtRPeDOfCuvwc9i3pZ2OPh%2Fhomepage-hero-asset.jpg&w=3840&q=100&dpl=dpl_8WtT7My4xK1Q2Zqrh8drueL4PDsy" alt="">
     <div>
-      <div class="bg-white font-normal text-sm rounded-full py-1 mt-2 Roboto px-5 text-center w-52 mx-auto">See how Basehub Works</div>
+      <div class="bg-white font-normal text-sm rounded-full py-1 mt-5 Roboto px-5 text-center w-52 mx-auto">See how Basehub Works</div>
     </div>
    </div>
 </main>
