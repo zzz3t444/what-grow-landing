ba886ab1422a37c0033216c3d24ec77ecaf5cf09
Zeeta:) <126560008+zzz3t444@users.noreply.github.com>
2024-09-23 18:53:40 +0700

    <ui>(Update Banner Section)

---
+++ src/section/Banner/BannerInformation.svelte
@@ -0,0 +1,22 @@
+<section>
+  <main>
+    <div class="mt-36">
+      <h1 class="Manrope text-xl lg:text-3xl font-medium text-center tracking-tighter dark:text-neutral-200">Building blocks for your website</h1>
+      <p class="Roboto text-neutral-500 max-w-xl mx-auto lg:text-lg text-center mt-3">Work on a beautiful. Notion-ske document. Press */* and pick a block from a selection from over a dozen option</p>
+    </div>
+    <div>
+      <img
+        class="w-[1000px] mx-auto mt-10 mb-10 rounded-3xl border dark:border-neutral-800"
+        src="https://basehub.com/_next/image?url=https%3A%2F%2Fassets.basehub.com%2F9dbc0da9%2Fgdz9j1iNS5IGiSZiE_Oci%2Fdeliver-your-content-via-graphql-asset.png%3Fformat%3Davif%26quality%3D100&w=2048&q=100&dpl=dpl_BnkDfdwNrd6qASdGhKc8vovkuBda"
+        alt=""
+      />
+    </div>
+    <div class="flex justify-center">
+        <ul class="grid gap-3 text-neutral-500 text-sm tracking-tight lg:text-lg ">
+          <li>Model and write your content in one go, without the context switch.</li>
+          <li>Drag-and-drop, duplicate, multi-select Blocks for faster workflows.</li>
+          <li>Build Components to reuse, just like you’d do in Figma or in React.</li>
+        </ul>
+      </div>
+  </main>
+</section>
--- src/section/Banner/BannerSec.svelte
+++ src/section/Banner/BannerSec.svelte
@@ -7,7 +7,7 @@
     <h1 class="Manrope text-xl lg:text-3xl font-medium text-center tracking-tighter dark:text-neutral-200">Building blocks for your website</h1>
     <p class="Roboto text-neutral-500 max-w-xl mx-auto lg:text-lg text-center mt-3">Work on a beautiful. Notion-ske document. Press */* and pick a block from a selection from over a dozen option</p>
     <div>
-      <img class="w-[1000px] mx-auto mt-10 mb-10 filter grayscale" src="https://assets.basehub.com/9dbc0da9/x47x8awtppea1jdzk67oxwjz/image-bento-2.png?format=webp&quality=100" alt="" />
+      <img class="w-[1000px] mx-auto mt-10 mb-10 rounded-3xl border dark:border-neutral-800" src="https://basehub.com/_next/image?url=https%3A%2F%2Fassets.basehub.com%2F9dbc0da9%2F2y_z4aSvHv8zoTcH-VgSH%2Fbuilding-blocks-for-your-website-asset.jpg%3Fformat%3Davif%26quality%3D100&w=2048&q=100&dpl=dpl_BnkDfdwNrd6qASdGhKc8vovkuBda" alt="" />
     </div>
     <div class="flex justify-center">
       <ul class="grid gap-3 text-neutral-500 text-sm tracking-tight lg:text-lg ">
